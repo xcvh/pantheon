959aec74c5de3f937ebdda2c513e4455f1e4d11c
xcvh <hi@jel.do>
2024-09-17 12:25:55 +0200

    Missing ; in flake.nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
-    }
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
