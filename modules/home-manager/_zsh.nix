dbac6c27e25d2805e5e1ecc7988dcf6162f87af1
xcvh <hi@jel.do>
2024-10-01 16:04:00 +0200

    feat: Add NVM and Herd config to zsh initialization.

--- modules/home-manager/_zsh.nix
+++ modules/home-manager/_zsh.nix
@@ -11,6 +11,11 @@
     };
     # inspo: https://discourse.nixos.org/t/brew-not-on-path-on-m1-mac/26770/4
     initExtra = ''
+      export NVM_DIR="/Users/xcvh/Library/Application Support/Herd/config/nvm"
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+      [[ -f "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh" ]] && builtin source "/Applications/Herd.app/Contents/Resources/config/shell/zshrc.zsh"
+
       if [[ $(uname -m) == 'arm64' ]] && [[ $(uname -s) == 'Darwin' ]]; then
         eval "$(/opt/homebrew/bin/brew shellenv)"
       fi
