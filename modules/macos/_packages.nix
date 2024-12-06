1ef572e768534c0c57246b4494b0ccd7169ab014
xcvh <hi@jel.do>
2024-12-06 15:50:27 +0100

    refactor: remove outdated and problematic packages
    Remove "Outlook",Gestimer 2", "Stando", "tor-browser", and 
    "microsoft-outlook" from the package list due to download errors 
    and outdated status. This streamlines the package management 
    and prevents installation issues.

--- modules/macos/_packages.nix
+++ modules/macos/_packages.nix
@@ -57,7 +57,6 @@
       "kiibohd-configurator"
       "mullvadvpn"
       "microsoft-remote-desktop"
-      # "microsoft-outlook"
       "microsoft-teams"
       "numi"
       "ollama"
@@ -73,7 +72,6 @@
       "font-scientifica"
       "font-cozette"
       "syncthing" # this should probably live in home-manager
-      "tor-browser"
       "whatsapp"
       "vlc"
       "zed"
@@ -85,9 +83,6 @@
       "ColorSlurp" = 1287239339;
       "Front and Center" = 1493996622;
       "OneDrive" = 823766827;
-      # "Gestimer 2" = 2142808944;
-      # "Stando" = 2134630863;
-      # These Give the Error: Download request failed: The server gave an error during download: 500 Internal Server Error.
       "Tailscale" = 1475387142;
       "TextSniper" = 1528890965;
       "Things 3" = 904280696;
@@ -95,7 +90,6 @@
       "Slack" = 803453959;
       "MultiMarkdown Composer 4" = 1275176220;
       "In Your Face" = 1476964367;
-      "Outlook" = 985367838;
       "Whisper Transcription" = 1668083311;
     };
   };
